9fffa992197ade45b0a58682bdd65b374e495cef
M-OssaOliveira <molekamari@gmail.com>
2025-12-19 17:21:13 -0300

    está dando erro, antes whitelabelerror, agora parou de compilar depois de inicializar o springdoc

--- demo-park-api/src/main/java/com/mballem/demoparkapi/config/SpringDocOpenAPIConfig.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/config/SpringDocOpenAPIConfig.java
@@ -1,4 +1,18 @@
 package com.mballem.demoparkapi.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringDocOpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("REST API_ Spring Park").description("API para gestão e estacionamento de veículos").version("V1").license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")).contact(new Contact().name("Mariana Oliv").email("moliv@spring-park.com")));
+    }
+
 }
