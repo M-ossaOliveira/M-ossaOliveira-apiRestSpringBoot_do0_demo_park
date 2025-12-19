4b42d04fd55135d0087f3025e84a5b8336467ca8
M-OssaOliveira <molekamari@gmail.com>
2025-12-19 14:41:49 -0300

    tratamento de excecoes com rest controller advice

--- demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.mballem.demoparkapi.service;
 
 import com.mballem.demoparkapi.entity.Usuario;
+import com.mballem.demoparkapi.exception.EntityNotFoundException;
+import com.mballem.demoparkapi.exception.UsernameUniqueViolationException;
 import com.mballem.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,13 +18,17 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        }catch(org.springframework.dao.DataIntegrityViolationException ex){
+           throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado",usuario.getUsername()));
+        }
     }
 
     @Transactional(readOnly = true)
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Usuário não encontrado.")
+                () -> new EntityNotFoundException(String.format("Usuário id: %s não encontrado",id))
         );
     }
 
