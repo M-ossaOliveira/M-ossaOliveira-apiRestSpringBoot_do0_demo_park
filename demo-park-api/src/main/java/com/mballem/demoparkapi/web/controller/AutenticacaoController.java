28f0f3385e7fd594387faa4d340d7c0877500cde
M-OssaOliveira <molekamari@gmail.com>
2025-12-23 11:50:09 -0300

    interceptacao requisicao cliente aula 54

--- demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/AutenticacaoController.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/AutenticacaoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
     private final JwtUserDetaisService detailsService;
     private final AuthenticationManager authenticationManager;
+    @PostMapping("/auth")
     public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto dto,
                                                HttpServletRequest request){
         log.info("Processo de autenticação pelo logim {}", dto.getUsername());
